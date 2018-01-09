7aca74a7f4f2ee97422e929d77713fc427f4c267
HaoJun <rin841005@gmail.com>
2018-01-09 10:43:56 +0800

    alertDialog done

--- app/src/main/java/idv/haojun/floatingwindowdemo/MainActivity.java
+++ app/src/main/java/idv/haojun/floatingwindowdemo/MainActivity.java
@@ -1,13 +1,47 @@
 package idv.haojun.floatingwindowdemo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_alert_dialog).setOnClickListener(this);
+        findViewById(R.id.tv_custom_alert_dialog).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_alert_dialog:
+                alertDialog();
+                break;
+            case R.id.tv_custom_alert_dialog:
+                customAlertDialog();
+                break;
+        }
+    }
+
+    private void alertDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.title)
+                .setMessage(R.string.message)
+                .setPositiveButton(R.string.good, null)
+                .setNegativeButton(R.string.bad, null)
+                .setNeutralButton(R.string.fine, null)
+                .show();
+    }
+
+    private void customAlertDialog() {
+
+    }
+
+
 }
