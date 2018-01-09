f8434e9c4bcf101fe1d8135a0b63f61c269866a2
HaoJun <rin841005@gmail.com>
2018-01-09 23:49:02 +0800

    popupWindow done

--- app/src/main/java/idv/haojun/floatingwindowdemo/MainActivity.java
+++ app/src/main/java/idv/haojun/floatingwindowdemo/MainActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.tv_alert_dialog).setOnClickListener(this);
         findViewById(R.id.tv_custom_dialog_by_alert_dialog).setOnClickListener(this);
         findViewById(R.id.tv_custom_dialog_by_activity).setOnClickListener(this);
+        findViewById(R.id.tv_popup_window).setOnClickListener(this);
     }
 
     @Override
@@ -32,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.tv_custom_dialog_by_activity:
                 startActivity(new Intent(this, CustomDialogActivity.class));
+                break;
+            case R.id.tv_popup_window:
+                popupWindow(v);
                 break;
         }
     }
@@ -77,5 +84,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    private void popupWindow(View clickView) {
+        final View v = LayoutInflater.from(this).inflate(R.layout.popup_window, null);
+        final PopupWindow popupWindow = new PopupWindow(v, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        v.findViewById(R.id.ll_popup_window_share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+        v.findViewById(R.id.ll_popup_window_look).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+        v.findViewById(R.id.ll_popup_window_comment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setAnimationStyle(R.style.AppTheme_PopWindowDefault);
+        popupWindow.showAtLocation(findViewById(R.id.rl_main), Gravity.CENTER, 0, 0);
+    }
 
 }
